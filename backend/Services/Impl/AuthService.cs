84cf1bfb0aa565a5c8948a345fcaff1e08030689
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-07-11 13:49:54 +0300

    refactor authservice

--- backend/Services/Impl/AuthService.cs
+++ backend/Services/Impl/AuthService.cs
@@ -5,93 +5,87 @@ using System.Text;
 using AutoMapper;
 using backend.Data;
 using backend.DTOs.Auth;
-using backend.DTOs.Cart;
-using backend.DTOs.CartItem;
-using backend.DTOs.Product;
 using backend.DTOs.User;
 using backend.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using BCrypt;
 
-namespace backend.Services.Impl;
-
-public class AuthService : IAuthService
+namespace backend.Services.Impl
 {
-    private readonly IMapper _mapper;
-    private readonly DataContext _context;
-    private readonly IConfiguration _configuration;
-
-    public AuthService(IMapper mapper, DataContext context, IConfiguration configuration)
+    public class AuthService : IAuthService
     {
-        _mapper = mapper;
-        _context = context;
-        _configuration = configuration;
-    }
+        private readonly IMapper _mapper;
+        private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
 
-    public async Task<ServiceResponse<AuthUserRespDTO>> Login(AuthUserReqDTO request)
-    {
-        var serviceResponse = new ServiceResponse<AuthUserRespDTO>();
-        try
+        public AuthService(IMapper mapper, DataContext context, IConfiguration configuration)
         {
-            var dbUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
-            if (dbUser == null)
+            _mapper = mapper;
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public async Task<ServiceResponse<AuthUserRespDTO>> Login(AuthUserReqDTO request)
+        {
+            var serviceResponse = new ServiceResponse<AuthUserRespDTO>();
+            try
+            {
+                var dbUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == request.Email);
+                if (dbUser == null)
+                {
+                    serviceResponse.Success = false;
+                    serviceResponse.Message = "Email not found.";
+                    return serviceResponse;
+                }
+
+                if (!BCrypt.Net.BCrypt.Verify(request.Password, dbUser.Password))
+                {
+                    serviceResponse.Success = false;
+                    serviceResponse.Message = "Wrong password.";
+                    return serviceResponse;
+                }
+
+                var token = GenerateToken(dbUser);
+                var authResponse = new AuthUserRespDTO
+                {
+                    Token = token,
+                    Name = dbUser.Name,
+                    Email = dbUser.Email,
+                    Password = dbUser.Password,
+                    Initials = dbUser.Initials
+                };
+                serviceResponse.Data = authResponse;
+            }
+            catch (Exception ex)
             {
                 serviceResponse.Success = false;
-                serviceResponse.Message = "Email not found.";
-                return serviceResponse;
+                serviceResponse.Message = ex.Message;
             }
 
-            if (!BCrypt.Net.BCrypt.Verify(request.Password, dbUser.Password))
-            {
-                serviceResponse.Success = false;
-                serviceResponse.Message = "Wrong password.";
-                return serviceResponse;
-            }
-           
-            string token = CreateToken(dbUser);
-            var authResponse = new AuthUserRespDTO 
-            { 
-                Token = token ,
-                Name = dbUser.Name,
-                Email = dbUser.Email,
-                Password = dbUser.Password,
-                Initials = dbUser.Initials
-            };
-            serviceResponse.Data = authResponse;
+            return serviceResponse;
         }
-        catch (Exception ex)
+
+        private string GenerateToken(User user)
         {
-            serviceResponse.Success = false;
-            serviceResponse.Message = ex.Message;
-        }
-       
-        return serviceResponse;
-    }
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.UTF8.GetBytes(_configuration["AppSettings:Token"]);
 
-    private string CreateToken(User user)
-    {
-        List<Claim> claims = new List<Claim> {
-                new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.Role, "Admin"),
-                new Claim(ClaimTypes.Role, "Customer"),
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Role, "Admin"),
+                    new Claim(ClaimTypes.Role, "Customer")
+                }),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha512)
             };
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-            _configuration.GetSection("AppSettings:Token").Value!));
-
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-        var token = new JwtSecurityToken(
-               claims: claims,
-               expires: DateTime.Now.AddDays(1),
-               signingCredentials: creds
-           );
-
-        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
-
-        return jwt;
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
