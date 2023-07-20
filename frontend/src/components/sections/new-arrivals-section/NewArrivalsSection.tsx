cc05e84e66cd641aaf9d31dd393ab0dd61a912b5
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-07-20 12:56:51 +0300

    added loading state and make dropdown menu close when a menu item is clicked

--- frontend/src/components/sections/new-arrivals-section/NewArrivalsSection.tsx
+++ frontend/src/components/sections/new-arrivals-section/NewArrivalsSection.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../hooks/reduxHook';
 import { fetchAllProducts } from '../../../redux/reducers/productSlice';
 import {
@@ -8,27 +8,36 @@ import {
   SectionNameContainer,
 } from '../new-arrivals-section/NewArrivalsSection.styles';
 import ProductCard from '../../product-card/ProductCard';
+import { CircularProgress } from '@mui/material';
 
 const NewArrivalsSection = () => {
   const products = useAppSelector((state) => state.productReducer);
   const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    dispatch(fetchAllProducts());
+    dispatch(fetchAllProducts())
+      .then(() => setLoading(false))
+      .catch(() => setLoading(false));
   }, [dispatch]);
+
   return (
     <SectionContainer maxWidth={false}>
       <SectionNameContainer>
         <SectionName variant="h6">New Arrivals</SectionName>
       </SectionNameContainer>
-      <ProductCardsContainer>
-        {products.length > 0 &&
-          products
-            .slice(1, 7)
-            .map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-      </ProductCardsContainer>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <ProductCardsContainer>
+          {products.length > 0 &&
+            products
+              .slice(1, 7)
+              .map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+        </ProductCardsContainer>
+      )}
     </SectionContainer>
   );
 };
